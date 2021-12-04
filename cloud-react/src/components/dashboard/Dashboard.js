76d968db73d3c904d122c7d12e5a9178cfd6ef55
Amlan Alok <amlanalok@gmail.com>
2021-12-04 17:08:29 -0600

    removed redundant code

--- cloud-react/src/components/dashboard/Dashboard.js
+++ cloud-react/src/components/dashboard/Dashboard.js
@@ -9,98 +9,7 @@ function Dashboard() {
     // const [userId, setUserId] = useState(props.userId)
     const {userId} = useParams();
     const [showChatList, setShowChatList] = useState(false)
-    // const [leftMenu, setLeftMenu] = useState(['Home', 'Chat'])
     const [chats, setChats] = useState([])
-    
-    // const chats1 = [    
-    //     {
-    //       "receiverId": 2,
-    //       "firstName": "Arya",
-    //       "lastName": "Stark",
-    //       "chatMessages": [
-    //         {
-    //           id: 1,
-    //           senderId: 1,
-    //           receiverId: 2,
-    //           message: "Hi Arya",
-    //           messageTimestamp: "2021-11-19T17:39:19.000+00:00"
-    //         },
-    //         {
-    //           id: 2,
-    //           senderId: 2,
-    //           receiverId: 1,
-    //           message: "Hi Jon",
-    //           messageTimestamp: "2021-11-19T17:39:19.000+00:00"
-    //         }
-    //         ]
-        
-    //     },
-    //     {
-    //       "receiverId": 3,
-    //       "firstName": "Sansa",
-    //       "lastName": "Stark",
-    //       "chatMessages": [
-    //           {
-    //             "id": 3,
-    //             "senderId": 1,
-    //             "receiverId": 3,
-    //             "message": "Hi Sansa",
-    //             "messageTimestamp": "2021-11-19T17:39:19.000+00:00"
-    //           },
-    //           {
-    //             "id": 4,
-    //             "senderId": 3,
-    //             "receiverId": 1,
-    //             "message": "Hi Jon",
-    //             "messageTimestamp": "2021-11-19T17:39:19.000+00:00"
-    //           }
-    //         ]
-    //     }
-    //     ]
-    // const chats2 = [    
-    //     {
-    //       "receiverId": 1,
-    //       "firstName": "Jon",
-    //       "lastName": "Snow",
-    //       "chatMessages": [
-    //         {
-    //           id: 1,
-    //           senderId: 1,
-    //           receiverId: 2,
-    //           message: "Hi Arya",
-    //           messageTimestamp: "2021-11-19T17:39:19.000+00:00"
-    //         },
-    //         {
-    //           id: 2,
-    //           senderId: 2,
-    //           receiverId: 1,
-    //           message: "Hi Jon",
-    //           messageTimestamp: "2021-11-19T17:39:19.000+00:00"
-    //         }
-    //         ]
-    //     },
-    //     {
-    //         "receiverId": 4,
-    //         "firstName": "Bran",
-    //         "lastName": "Stark",
-    //         "chatMessages": [
-    //           {
-    //             id: 1,
-    //             senderId: 4,
-    //             receiverId: 2,
-    //             message: "Hi Arya",
-    //             messageTimestamp: "2021-11-19T17:39:19.000+00:00"
-    //           },
-    //           {
-    //             id: 2,
-    //             senderId: 2,
-    //             receiverId: 4,
-    //             message: "Hi Bran",
-    //             messageTimestamp: "2021-11-19T17:39:19.000+00:00"
-    //           }
-    //           ]
-    //       }
-    //     ]
 
     useEffect(() => {
         console.log('Loading Dashboard')
@@ -118,14 +27,6 @@ function Dashboard() {
             }
         })
 
-        // if (userId == 1){
-        //     console.log('userId = 1')
-        //     setChats(chats1)
-        // }
-        // else {
-        //     console.log('userId = 2')
-        //     setChats(chats2)
-        // }
     }, []);
 
     
